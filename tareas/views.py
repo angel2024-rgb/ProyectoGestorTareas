8818391a994fc82286257d28ceb8a8e743aff5f2
Angel Paredes <amanuelparedess@gmail.com>
2026-03-12 14:33:20 -0500

    login, registro y dashboard funcionando

--- tareas/views.py
+++ tareas/views.py
@@ -37,6 +37,14 @@ class TareaDetalleView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def patch(self, request, pk):
+        tarea = get_object_or_404(Tarea, pk=pk, usuario=request.user)
+        serializer = TareaSerializer(tarea, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk):
         tarea = get_object_or_404(Tarea, pk=pk, usuario=request.user)
