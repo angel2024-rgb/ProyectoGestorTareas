933fed7d5d2dea08f5da98777f4cdea944b1ee8c
Angel Paredes <amanuelparedess@gmail.com>
2026-04-28 15:01:02 -0500

    actualización de versión de psycopg y de dj-database-url

--- ProyectoGestorTareas/settings.py
+++ ProyectoGestorTareas/settings.py
@@ -88,16 +88,12 @@ WSGI_APPLICATION = 'ProyectoGestorTareas.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-""" DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-} """
+
 DATABASES = {
     'default': dj_database_url.config(
         default='sqlite:///db.sqlite3',
-        conn_max_age=600
+        conn_max_age=600,
+        conn_health_checks=True,
     )
 }
 
