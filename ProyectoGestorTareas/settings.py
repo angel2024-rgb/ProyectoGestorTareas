0fe49be0321bdaf67e707eb0d8f26dd6c2c5c2b4
Angel Paredes <amanuelparedess@gmail.com>
2026-04-28 00:22:12 -0500

    Cambiar de SQLite a PostgreSQL

--- ProyectoGestorTareas/settings.py
+++ ProyectoGestorTareas/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 from pathlib import Path
 from dotenv import load_dotenv
 import os
+import dj_database_url
 
 # Cargar variables del archivo .env (solo para desarrollo local)
 load_dotenv()
@@ -87,13 +88,18 @@ WSGI_APPLICATION = 'ProyectoGestorTareas.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
+""" DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+} """
+DATABASES = {
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600
+    )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
